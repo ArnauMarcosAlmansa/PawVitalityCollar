50aad5b04fb6c55276b535c0e8e4ba372fe8eeb2
Oriol Marión <oriolmarionescude@gmail.com>
2023-11-01 14:23:27 +0100

    App crashes when bluetooth is disabled

--- PawVitalityApp/app/src/main/java/com/example/pawvitalityapp/presentation/SensorsScreen.kt
+++ PawVitalityApp/app/src/main/java/com/example/pawvitalityapp/presentation/SensorsScreen.kt
@@ -1,12 +1,19 @@
 package com.example.pawvitalityapp.presentation
 
+import android.bluetooth.BluetoothAdapter
 import androidx.compose.runtime.Composable
+import com.example.pawvitalityapp.presentation.permissions.SystemBroadcastReceiver
 
 @Composable
 fun SensorsScreen(
-
+    onBluetoothStateChanged:()->Unit
 ) {
 
-
+    SystemBroadcastReceiver(systemAction = BluetoothAdapter.ACTION_STATE_CHANGED){ bluetoothState ->
+        val action = bluetoothState?.action ?: return@SystemBroadcastReceiver
+        if(action == BluetoothAdapter.ACTION_STATE_CHANGED){
+            onBluetoothStateChanged()
+        }
+    }
 
 }
