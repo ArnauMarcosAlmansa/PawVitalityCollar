4042e98daeda74093087e38f54617ca6bf77408b
OriolMarion <oriolmarionescude@gmail.com>
2023-11-13 11:09:24 +0100

    firebase plugins, dependencies and json file

--- PawVitalityApp/app/build.gradle.kts
+++ PawVitalityApp/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     kotlin("kapt")
     id("com.google.dagger.hilt.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -107,6 +108,19 @@ dependencies {
 
     //Permissions
     implementation("com.google.accompanist:accompanist-permissions:0.32.0")
+
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
+
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-database-ktx")
+    implementation("com.google.firebase:firebase-functions-ktx")
+
+
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
 }
 
 // Allow references to generated code
