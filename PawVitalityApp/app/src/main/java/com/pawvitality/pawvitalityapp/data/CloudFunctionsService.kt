1d0d4a5e2599b7fe3196781ba2b3b915c022c1fd
Arnau Marcos Almansa <arnaumarcosalmansa@gmail.com>
2023-12-11 11:19:13 +0100

    parsear los datos de la cloud function

--- PawVitalityApp/app/src/main/java/com/pawvitality/pawvitalityapp/data/CloudFunctionsService.kt
+++ PawVitalityApp/app/src/main/java/com/pawvitality/pawvitalityapp/data/CloudFunctionsService.kt
@@ -33,17 +33,13 @@ class CloudFunctionsService {
             .continueWith { task ->
                 val result = task.result?.data as Map<*, *>
 
-                val resultTemperature = result["temperature"] as Map<String, Float>
-                val tmp = TemperatureData()
-                tmp.current = (resultTemperature["current"] as Float)
-                tmp.resting = (resultTemperature["resting"] as Float)
-                tmp.high = (resultTemperature["high"] as Float)
+                val resultTemperature = result["temperature"] as Map<String, *>
+                val tmp = parseTemperature(resultTemperature)
+//                tmp.resting = (resultTemperature["resting"]?.toFloat() ?: 0.0f)
+//                tmp.high = (resultTemperature["high"]?.toFloat() ?: 0.0f)
 
                 val resultHeartRate = result["heartRate"] as Map<String, Float>
-                val hrt = HeartRateData()
-                hrt.current = (resultHeartRate["current"] as Float)
-                hrt.resting = (resultHeartRate["resting"] as Float)
-                hrt.high = (resultHeartRate["high"] as Float)
+                val hrt = parseHeartRate(resultHeartRate)
 
                 Log.d("FEEDBACK", "success")
 
@@ -51,5 +47,67 @@ class CloudFunctionsService {
             }.addOnFailureListener {
                 Log.e("FEEDBACK", "exception", it)
             }
+    }
+
+    fun parseTemperature(resultTemperature: Map<String, *>): TemperatureData {
+        val tmp = TemperatureData()
+
+        Log.d("FEEDBACK", resultTemperature.toString())
+        if (resultTemperature["current"] == null) {
+            tmp.current = 0.0f;
+        } else if (resultTemperature["current"] is Int?) {
+            tmp.current = (resultTemperature["current"] as Int).toFloat()
+        } else {
+            tmp.current = (resultTemperature["current"] as Double).toFloat()
+        }
+
+        if (resultTemperature["high"] == null) {
+            tmp.high = 0.0f;
+        } else if (resultTemperature["high"] is Int?) {
+            tmp.high = (resultTemperature["high"] as Int).toFloat()
+        } else {
+            tmp.high = (resultTemperature["high"] as Double).toFloat()
+        }
+
+        if (resultTemperature["resting"] == null) {
+            tmp.resting = 0.0f;
+        } else if (resultTemperature["resting"] is Int?) {
+            tmp.resting = (resultTemperature["resting"] as Int).toFloat()
+        } else {
+            tmp.resting = (resultTemperature["resting"] as Double).toFloat()
+        }
+
+        return tmp
+    }
+
+    fun parseHeartRate(resultHeartRate: Map<String, *>): HeartRateData {
+        val hr = HeartRateData()
+
+        Log.d("FEEDBACK", resultHeartRate.toString())
+        if (resultHeartRate["current"] == null) {
+            hr.current = 0.0f;
+        } else if (resultHeartRate["current"] is Int?) {
+            hr.current = (resultHeartRate["current"] as Int).toFloat()
+        } else {
+            hr.current = (resultHeartRate["current"] as Double).toFloat()
+        }
+
+        if (resultHeartRate["high"] == null) {
+            hr.high = 0.0f;
+        } else if (resultHeartRate["high"] is Int?) {
+            hr.high = (resultHeartRate["high"] as Int).toFloat()
+        } else {
+            hr.high = (resultHeartRate["high"] as Double).toFloat()
+        }
+
+        if (resultHeartRate["resting"] == null) {
+            hr.resting = 0.0f;
+        } else if (resultHeartRate["resting"] is Int?) {
+            hr.resting = (resultHeartRate["resting"] as Int).toFloat()
+        } else {
+            hr.resting = (resultHeartRate["resting"] as Double).toFloat()
+        }
+
+        return hr
     }
 }
